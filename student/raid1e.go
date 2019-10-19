a8308bfee3154c1ac872e427fe8c3636eb11b710
armanospan <arman.ospan@yahoo.com>
2019-10-19 16:15:13 +0600

    raid1 solutions

---
+++ student/raid1e.go
@@ -0,0 +1,43 @@
+package student
+
+import "github.com/01-edu/z01"
+
+// Raid1e function prints a square
+// of width x and of height y
+func Raid1e(x, y int) {
+	if x > 0 && y > 0 {
+		z01.PrintRune('A')
+
+		if y >= 1 {
+			for i := 0; i < (x - 2); i++ {
+				z01.PrintRune('B')
+			}
+			if x >= 2 {
+				z01.PrintRune('C')
+			}
+			z01.PrintRune('\n')
+
+			for j := 0; j < (y - 2); j++ {
+				z01.PrintRune('B')
+				for i := 0; i < (x - 2); i++ {
+					z01.PrintRune(' ')
+				}
+				if x >= 2 {
+					z01.PrintRune('B')
+				}
+				z01.PrintRune('\n')
+			}
+
+			if y >= 2 {
+				z01.PrintRune('C')
+				for i := 0; i < (x - 2); i++ {
+					z01.PrintRune('B')
+				}
+				if x >= 2 {
+					z01.PrintRune('A')
+				}
+				z01.PrintRune('\n')
+			}
+		}
+	}
+}
