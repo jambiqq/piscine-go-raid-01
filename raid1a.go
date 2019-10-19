847d25feeaab05feb39c133d5f42a657443288dd
armanospan <arman.ospan@yahoo.com>
2019-10-19 16:13:10 +0600

    raid1a

---
+++ raid1a.go
@@ -0,0 +1,43 @@
+package student
+
+import "github.com/01-edu/z01"
+
+// Raid1a function prints a square
+// of width x and of height y
+func Raid1a(x, y int) {
+	if x > 0 && y > 0 {
+		z01.PrintRune('o')
+
+		if y >= 1 {
+			for i := 0; i < (x - 2); i++ {
+				z01.PrintRune('-')
+			}
+			if x >= 2 {
+				z01.PrintRune('o')
+			}
+			z01.PrintRune('\n')
+
+			for j := 0; j < (y - 2); j++ {
+				z01.PrintRune('|')
+				for i := 0; i < (x - 2); i++ {
+					z01.PrintRune(' ')
+				}
+				if x >= 2 {
+					z01.PrintRune('|')
+				}
+				z01.PrintRune('\n')
+			}
+
+			if y >= 2 {
+				z01.PrintRune('o')
+				for i := 0; i < (x - 2); i++ {
+					z01.PrintRune('-')
+				}
+				if x >= 2 {
+					z01.PrintRune('o')
+				}
+				z01.PrintRune('\n')
+			}
+		}
+	}
+}
